14a05ca627410e8711fff459ba1d2a513b047e25
nroshdy92 <156032373+nroshdy92@users.noreply.github.com>
2024-01-10 18:39:25 -0500

    Create todayclass.tf

---
+++ todayclass.tf
@@ -0,0 +1,16 @@
+variable "simple_nested_list" {​
+
+  type    = list(list(string))​
+
+  default = [["a", "b"], ["c", "d"], ["e", "f"]]​
+
+}
+locals{​
+
+simple_local_flattened_list = flatten(var.simple_nested_list)}​
+
+output "simple_flattened_list_result" {​
+
+  value = local.simple_local_flattened_list​
+
+}​
